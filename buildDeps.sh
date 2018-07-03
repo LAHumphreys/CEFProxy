7b3ed7ea0cd13a6fa517035e3815e1d0046a6a0e
Luke Humphreys <lhumphreys67@gmail.com>
2018-07-03 21:24:29 +0100

    Build configuration

---
+++ buildDeps.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#TODO
+#if [[ -e CPPWebSocketResponseRequestConfig.cmake ]]; then
+    #echo "Building dependencies..."
+#else
+    #echo "This script should be run in the home directory of the project"
+    #exit 1
+#fi
+
+if [[ "$1" == "" ]]; then
+    DEPS_ROOT="$PWD/deps"
+else
+    DEPS_ROOT="$1"
+fi
+if [[ -e $DEPS_ROOT/CMakeUtils ]]; then
+    echo "Existing CMakeUtils directory, no need to clone"
+else
+    git clone https://github.com/Grauniad/CMakeUtils.git $DEPS_ROOT/CMakeUtils || exit 1
+fi
+
+declare -A depList
+
+depList[CEFBaseApp]=https://github.com/Grauniad/CEFBaseApp.git
+
+source $DEPS_ROOT/CMakeUtils/build_tools/buildDepsCommon.sh || exit 1
